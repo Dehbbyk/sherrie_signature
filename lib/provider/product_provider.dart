21788760056109ccf30b81762a861c9f324ab22e
HP <deborah.oyinda@yahoo.com>
2024-07-09 17:04:38 +0100

    Modification on the UI to fix errors.

--- lib/provider/product_provider.dart
+++ lib/provider/product_provider.dart
@@ -6,19 +6,4 @@ class ProductProvider with ChangeNotifier {
   bool _isLoading = false;
   List<dynamic> get product => _product;
   bool get isLoading => _isLoading;
-  // Future<void> fetchProduct() async {
-  //   _isLoading = true;
-  //   notifyListeners();
-  //   try {
-  //     _product = await apiService.fetchProduct();
-  //     notifyListeners();
-  //   } catch (error) {
-  //     debugPrint('Error fetching product: $error');
-  //     _product = [];
-  //   } finally {
-  //     _isLoading = false;
-  //     notifyListeners();
-  //     debugPrint('Loading state: $_isLoading');
-  //   }
-  // }
 }
